e2e21d67dcb31121f230ed9525b352dc14b5f224
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-16 15:08:09 +0900

    [3-2] 큰 수의 법칙
    * 배열의 크기 N, 숫자가 더해지는 횟수 M, 그리고 K가 주어질 때 동빈이의
      큰 수의 법칙에 따른 결과를 출력하시오
    * 3_2_1: 간단한 방법 M, K가 10,000보다 작을 때 가능
    * 3_2_2: 수열을 사용하여 M, K 크기에 영향을 받지 않는 방법

---
+++ Greedy/3_2_1.js
@@ -0,0 +1,40 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    const cond_string = input[0];
+    const list_string = input[1];
+
+    const N = parseInt(cond_string.substr(0, 1));
+    const M = parseInt(cond_string.substr(2, 1));
+    const K = parseInt(cond_string.substr(4, 1));
+
+    const list = [];
+    for(let i=0; i < list_string.length; i+=1) {
+        if (i % 2 === 0) list.push(parseInt(list_string.substr(i, 1)));
+    }
+
+    list.sort((a, b) => b - a);
+    
+    let result = 0;
+    let count = 0;
+    while(true) {
+        for (let i=0; i< K; i+=1) {
+            if (count === M) break;
+            result += list[0];
+            count += 1;
+        }
+        if (count === M) break;
+        result += list[1];
+        count +=1;
+    }
+    console.log(result);
+    process.exit();
+});
