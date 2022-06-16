13de7845838b3411614b614fdc86dccba2f9a613
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-16 11:57:46 +0900

    [3-1] 거스름돈
    * 카운터에는 거스름돈으로 사용할 500원, 100원, 50원, 10원짜리 동전이
      무한히 존재한다고 가정한다.
    * 손님에게 거슬러 줘야할 돈이 N원일 때 거슬러 줘야 할 동전의 최소 개수를
      구하라.
    * 단, 거슬러 줘야할 돈 N은 항상 10의 배수이다.

---
+++ Greedy/3_1.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", function (line) {
+    let N = Number(line);
+
+    let count = 0;
+    const coin_types = [500, 100, 50, 10];
+
+    coin_types.forEach(value => {
+        count += parseInt(N / value);
+        N %= value;
+    });
+    console.log(count);
+
+    rl.close();
+}).on("close", function () {
+    process.exit();
+});
