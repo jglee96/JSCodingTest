f0bd430be4471c81612242b87eaf64927dcfbae3
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-16 18:28:50 +0900

    [4-3] 왕실의 나이트
    * 8x8 좌표 평면상에서 나이트의 위치가 주어졌을 때 나이트가 이동할 수
      있는 경우의 수를 출력하는 프로그램을 작성하시오
    * 경로를 확인하여 유요한 경우만 count하여 출력

---
+++ Implementation/4_3.js
@@ -0,0 +1,32 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    const x = input[0].substr(0, 1).charCodeAt(0) - 96;
+    const y = parseInt(input[0].substr(1, 1));
+
+    const twoset = [2, -2]
+    const oneset = [1, -1];
+
+    let count = 0;
+    twoset.forEach(two => {
+        oneset.forEach(one => {
+            const ax = x + two;
+            const ay = y + one;
+            if (ax > 0 && ay > 0) count += 1;
+
+            const bx = x + one;
+            const by = y + two;
+            if (bx > 0 && by > 0) count += 1;
+        });
+    })
+    console.log(count);;
+    process.exit();
+});
