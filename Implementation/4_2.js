4ed81d5b492a27faceff48cf61436a5c0f6f77c2
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-16 17:42:17 +0900

    [4-2] 시각
    * 정수 N이 입력되면 00시 00분 00초부터 N시 59분 59초까지의 모든 시각
      중에서 3이 하나라도 포함되는 모든 경우의 수를 구하는 프로그램을
    작성하기오
    * 00시 00분 00초부터 23시 59분 59초까지 86400가지 밖에 되지 않으므로
      모두 확인해봐도됨
    * 완전 탐색(Brute Forcing)

---
+++ Implementation/4_2.js
@@ -0,0 +1,29 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    /*
+    * 00시 00분 00초부터 23시 59분 59초까지 24 * 60 * 60 = 86400 < 10000 가지임
+    * 따라서 모두 확인해도 성능 문제가 없음
+    */
+    const N = parseInt(input[0]);
+
+    let count = 0;
+    for(let h=0; h<=N; h+=1) {
+        for(let m=0; m<60; m+=1) {
+            for(let s=0; s<60; s+=1) {
+                const t = `${h}${m}${s}`;
+                if (t.indexOf('3') !== -1) count += 1;
+            }
+        }
+    }
+    console.log(count);
+    process.exit();
+});
