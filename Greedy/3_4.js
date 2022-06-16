11fd947e3b8f2ae59a11ba634ea322d3490a38db
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-16 16:47:34 +0900

    [3-4] 1이 될 때까지
    * N과 K가 주어질 때 N이 1이 될 때까지 1번 혹은 2전의 과정을 수행해야하는
      최소 횟수를 구하는 프로그램을 작성하기오
    * 나누는 것이 N을 가장 빨리 줄일 수 있음 -> 나눌 수 있으면 나눈다.

---
+++ Greedy/3_4.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    let [N, K] = input[0].split(" ");
+
+    let count = 0;
+    while (N > 1) {
+        if (N % K === 0)  {
+            N /= K;
+            count += 1;
+        } else {
+            const q = N % K;
+            N -= q;
+            count += q;
+        }
+    }
+    console.log(count);
+    process.exit();
+});
