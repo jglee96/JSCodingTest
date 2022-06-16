2f25f471527a8eb0ba8fcc6c5f94ad306e68dc11
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-16 16:11:21 +0900

    [3-3] 숫자 카드 게임
    * 카드들이 NxM 형태로 놓여있을 때, 게임의 룰에 맞게 카드를 뽑는
      프로그램을 만드시오
    * 각 행마다 가장 작은 수를 찾은 뒤에 그 수 중에서 가장 큰 수를 찾음

---
+++ Greedy/3_3.js
@@ -0,0 +1,35 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    const size_string = input[0];
+    const N = parseInt(size_string.substr(0, 1));
+    const M = parseInt(size_string.substr(2, 1));
+    
+    const cards = new Array(N);
+    for (let i=0; i < cards.length; i+=1) {
+        cards[i] = new Array(M);
+    }
+
+    for (let i=1; i<input.length; i+=1) {
+        for (let j=0; j<input[i].length; j+=2) {
+            cards[i-1][j / 2] = parseInt(input[i].substr(j, 1));
+        }
+    }
+
+    const min_list = [];
+    cards.forEach(value => {
+        min_list.push(Math.min(...value));
+    });
+    const result = Math.max(...min_list);
+    
+    console.log(result);
+    process.exit();
+});
