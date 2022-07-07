3a25fefc135c6e3ee2a8561b7409032c2c67f1a6
jglee96 <zakklee96@gmail.com>
2022-07-07 22:43:56 +0900

    [5-5] 2가지 방식으로 구현한 팩토리얼
    * 반복적
    * 재귀적

---
+++ DFS_BFS/5_5.js
@@ -0,0 +1,19 @@
+// 반복적으로 구현한 n!
+function factorial_iterative(n) {
+    let result = 1;
+    for (let i=1; i< n+1; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+// 재귀적으로 구현한 n!
+function factorial_recursive(n) {
+    if (n <= 1) return 1;
+    else {
+        return n * factorial_recursive(n-1);
+    }
+}
+
+console.log(`반복적으로 구현: ${factorial_iterative(5)}`);
+console.log(`재귀적으로 구현: ${factorial_recursive(5)}`);
